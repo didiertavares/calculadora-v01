79f562a24aebe3b6ab886d3f04c87ac856b2f8fe
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2025-07-02 17:30:18 -0300

    versão inicial 02.07.25

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("input")
+let number = input.value
+
+const resultBox = document.querySelector("p")
+
+let resultContent = 0
+
+function display()  {
+    resultBox.innerText = resultContent
+}
+
+function int1() {
+    document.querySelector
+
+}
+
+function addition() {
+    resultContent + number
+    display()
+}
+
+display()
+console.log(display)
