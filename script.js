430902f39d55e2793798acb0715a53627ce06399
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2025-07-05 12:05:02 -0300

    in progress: v02

--- script.js
+++ script.js
@@ -1,23 +1,92 @@
+// estado da aplicação
 const input = document.querySelector("input")
-let number = input.value
 
-const resultBox = document.querySelector("p")
+const p = document.querySelector("p")
 
-let resultContent = 0
+const inputContent = []
 
-function display()  {
-    resultBox.innerText = resultContent
-}
+let newEntry
+
 
 function int1() {
-    document.querySelector
-
+    newEntry = 1
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
 }
-
-function addition() {
-    resultContent + number
-    display()
+function int2() {
+    newEntry = 2
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
 }
-
-display()
-console.log(display)
+function int3() {
+    newEntry = 3
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int4() {
+    newEntry = 4
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int5() {
+    newEntry = 5
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int6() {
+    newEntry = 6
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int7() {
+    input.value =newEntry = 7
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int8() {
+    newEntry = 8
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int9() {
+    newEntry = 9
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function int0() {
+    newEntry = 0
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function comma() {
+    newEntry = "."
+    inputContent.push(newEntry)
+    console.log(inputContent)
+    input.value = inputContent.join("")
+    console.log(inputContent.join(""))
+}
+function addition()  {
+    newEntry = ('  +  ')
+    input.value = inputContent.join("") + newEntry
+    console.log(input.value)
+}
