04759d9d13ea25c993efa813e5ab611417dc5798
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2025-09-01 17:48:06 -0300

    término aula 01/09 > in progress

--- script.js
+++ script.js
@@ -1,97 +1,80 @@
 // estado da aplicação
-const input = document.querySelector("input")
+const display = document.querySelector("p")
+const keypad = document.querySelectorAll('.key')
+const mathOps = document.querySelectorAll('.op')
+const button = document.querySelector('button')
+const enterButton = document.getElementById('enter')
 
-const p = document.querySelector("p")
+let result
+const data = []
+let dataFormatted
 
-const inputContent = []
+let savedData1
+let savedData2
 
-let newEntry
+let keyValue
+let operValue
 
-let data1
-let data2
+keypad.forEach(button => {
+    button.addEventListener('click', function() {
+        keyValue = this.value
+        console.log('clicked button value:', keyValue)
+        data.push(keyValue)
+        console.log(data)
+        dataFormatted = Number(data.join(''))
+        display.innerText = dataFormatted
+        console.log(typeof dataFormatted)
+        savedData1 = dataFormatted
+        console.log(savedData1)
+    })
+})
 
+mathOps.forEach(button => {
+    button.addEventListener('click', function() {
+        
+        data.length = 0
+        console.log(data)
+        operValue = this.value
+        display.innerHTML = operValue
+        console.log('clicked button value:', operValue)
 
-function int1() {
-    newEntry = 1
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
+    })
+})
+
+function calculus() {
+    if (operValue === '+') {
+        result = savedData1 + dataFormatted
+    } else if (operValue === "-") {
+        result = savedData1 - dataFormatted
+    } else if (operValue === "*") {
+        result = savedData1 * dataFormatted
+    } else {
+        if (dataFormatted === 0) {
+            result = "Error"
+        } else {
+            result = savedData1 / dataFormatted
+        }
+    }
+    console.log(result)
+    display.innerText = result
+    savedData1 = result
+    data.length = 0
+    console.log(data)
 }
-function int2() {
-    newEntry = 2
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int3() {
-    newEntry = 3
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int4() {
-    newEntry = 4
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int5() {
-    newEntry = 5
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int6() {
-    newEntry = 6
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int7() {
-    input.value =newEntry = 7
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int8() {
-    newEntry = 8
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int9() {
-    newEntry = 9
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function int0() {
-    newEntry = 0
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function decimalSeparator() {
-    newEntry = "."
-    inputContent.push(newEntry)
-    console.log(inputContent)
-    input.value = inputContent.join("")
-    console.log(inputContent.join(""))
-}
-function addition()  {
-    newEntry = ('  +  ')
-    data1 = inputContent.join("")
-    input.value = newEntry
-    p.innerText = data1
-    console.log(input.value)
-}
+
+enterButton.addEventListener('click', calculus)
+
+// function decimalSeparator() {
+//     newEntry = "."
+//     inputContent.push(newEntry)
+//     console.log(inputContent)
+//     input.value = inputContent.join("")
+//     console.log(inputContent.join(""))
+// }
+// function addition()  {
+//     newEntry = ('  +  ')
+//     data1 = inputContent.join("")
+//     input.value = newEntry
+//     p.innerText = data1
+//     console.log(input.value)
+// }
